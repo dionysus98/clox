81eeacc5c747677fdbc3fb5b2fdf99597588fea7
Avinash S <avinashthebigboss@gmail.com>
2024-10-05 22:12:18 +0530

    add | basic test

--- src/clox/play.clj
+++ src/clox/play.clj
@@ -1,15 +1,32 @@
-2 (ns clox.play
-    (:require [clox.interpreter :as intr]
-              [clox.lexer :as lex]
-              [clox.parser :as psr]))
+ (ns clox.play
+  (:require [clox.interpreter :as intr]
+            [clox.lexer :as lex]
+            [clox.parser :as psr]))
 
-(let [tks   (->> "print (7 + 8) * (7 * (8 + 9));"
-                 lex/lexer:new
-                 lex/lex
-                 :lexer/tokens)
-      stmts (->> tks
-                 psr/parser:new
-                 psr/parse
-                 :parser/stmts)
-      intr (intr/interpreter:new stmts)]
-  (intr/interpret intr))
+(let [print-op nil]
+  (binding [*out* (proxy [java.io.Writer] []
+                    (close [] nil)
+                    (flush [] nil)
+                    (write
+                      ([cbuf] (when-not print-op (set! print-op cbuf)))
+                      ([cbuf off len] nil)))]
+    (let [src   "var a = (7 + 8) * (7 * (8 + 9)); 
+             print a;"
+          tks   (->> src
+                     lex/lexer:new
+                     lex/lex
+                     :lexer/tokens)
+          stmts (->> tks
+                     psr/parser:new
+                     psr/parse
+                     :parser/stmts)
+          intr  (intr/interpreter:new stmts)]
+      (intr/interpret intr)
+      print-op)))
+
+
+(with-out-str (println "a"))
+
+*out*
+
+java.io.Reader
--- test/clox/main_test.clj
+++ test/clox/main_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer [deftest is testing]]
             [clox.ast :as ast]
             [clox.ast-test :as printer]
+            [clox.interpreter :as intr]
+            [clox.lexer :as lex]
+            [clox.parser :as psr]
             [clox.token :as t]))
 
 (deftest ast-test
@@ -15,6 +18,40 @@
           ast   (printer/->AstPrinter bin)
           env   {}]
       (is (= (.print! ast env) "(* (- 123) (group 45.67))")))))
+
+(deftest basic-print-test
+  (testing "Basic print statement | tokens, statements"
+    (let [src   "var a = (7 + 8) * (7 * (8 + 9));
+                 print a;"
+          tks   (->> src
+                     lex/lexer:new
+                     lex/lex
+                     :lexer/tokens)
+          stmts (->> tks
+                     psr/parser:new
+                     psr/parse
+                     :parser/stmts)]
+      (is (= 23 (count tks)))
+      (is (= 1 (:token/line (first tks))))
+      (is (= 2 (:token/line (last tks))))
+      (is (= :VAR (:token/kind (first tks))))
+      (is (= :EOF (:token/kind (last tks))))
+      (is (= 2 (count stmts)))
+      (is (instance? clox.ast.Var (first stmts)))
+      (is (instance? clox.ast.Print (last stmts)))
+      (testing "Basic print statement | interpret"
+        (let [!print-op (atom nil)]
+          (binding [*out* (proxy [java.io.Writer] []
+                            (close [] nil)
+                            (flush [] nil)
+                            (write
+                              ([cbuf] (swap! !print-op #(or % cbuf)))
+                              ([cbuf off len] nil)))]
+            (let [intr (intr/interpret (intr/interpreter:new stmts))]
+              (is (= (:env/values (:interpreter/env intr)) {"a" 1785.0}))
+              (is (empty? (:interpreter/errors intr)))
+              (is (false? (:interpreter/runtime-error? intr)))
+              (is (= "1785.0" @!print-op)))))))))
 
 (comment
   (test #'ast-test)
